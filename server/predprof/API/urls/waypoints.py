3c4ae6992780f106c9fdef24c750b75670019fd3
Timur <kitanin.ser@mail.ru>
2023-03-19 13:14:27 +0300

    final

--- server/predprof/API/urls/waypoints.py
+++ server/predprof/API/urls/waypoints.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from API.views import get_init_waypoints, get_days, get_waypoints, get_consumptions
+from API.views import get_init_waypoints, get_days, get_waypoints, get_consumptions, get_calcs
 
 urlpatterns = [
     path('get_init_waypoints/', get_init_waypoints.as_view()),
     path('get_waypoints/', get_waypoints.as_view()),
     path('get_consumptions/', get_consumptions.as_view()),
+    path('get_calcs/', get_calcs.as_view()),
     path('get_days/', get_days.as_view())
 ]
--- server/predprof/API/views.py
+++ server/predprof/API/views.py
@@ -4,12 +4,14 @@ from API.serializers import DaySerializer
 from rest_framework import generics
 from django.http import JsonResponse
 import requests
-
+from django.db import connection
+from math import *
 
 class get_init_waypoints(generics.GenericAPIView):
 
     def get(self, request):
-        Waypoint.objects.all().delete()
+        cursor = connection.cursor()
+        cursor.execute('TRUNCATE TABLE "API_waypoint" RESTART IDENTITY CASCADE')
         headers = {'X-Auth-Token': '7vt3qmhc'}
         response = requests.get('https://dt.miet.ru/ppo_it_final', headers=headers)
         for i in response.json()['message'][0]['points']:
@@ -47,8 +49,56 @@ class get_consumptions(generics.GenericAPIView):
         return JsonResponse(res, safe=False)
 
 
-class get_cals(generics.GenericAPIView):
+class get_calcs(generics.GenericAPIView):
 
 
     def get(self, request):
-        pass
+        cursor = connection.cursor()
+        cursor.execute('TRUNCATE TABLE "API_day" RESTART IDENTITY CASCADE')
+        waypoints = list(Waypoint.objects.all().values('SH', 'distance'))
+        points = []
+        for i in waypoints:
+            points.append([i['distance'], i['SH']])
+        days = 0
+        credits = 0
+        raspisanie = [{'SH_end': 8}]
+        n = 0
+        for s, c in points:
+            n += 1
+            for w in [80, 30, 5]:
+                t = s * (200 + c) / (5 * w)
+                if t >= log(1 + (c / 8), 2):
+                    break
+            t = ceil(t)
+            days += t
+
+            credits += 10 * w
+            if 11 * w >= 465:
+                oxi = 20
+                temp = 30
+                e = 465 / 11
+            elif 11 * w >= 325:
+                oxi = 30
+                temp = 25
+                e = 325 / 11
+            else:
+                oxi = 60
+                temp = 10
+                e = 55 / 11
+            credits += 7 * oxi * t
+
+            for i in range(t):
+                sh = 8 + 8 * (i + 1)
+                Day.objects.create(fuel_consumption=w,
+                                   engine_consumption=w - e,
+                                   electricity_consumption=e,
+                                   electricity=11 * e,
+                                   ship_mass=192 + sh,
+                                   oxygen_consumption=oxi * sh,
+                                   SH_start=raspisanie[-1]['SH_end'],
+                                   SH_end=sh,
+                                   temperature=temp,
+                                   waypoint_id=n)
+
+        return JsonResponse(True ,safe=False)
+
